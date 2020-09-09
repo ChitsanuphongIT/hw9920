b0ad775f303b3590e3a88643ff727e5893a2f2c6
ChitsanuphongIT <67995734+ChitsanuphongIT@users.noreply.github.com>
2020-09-09 16:28:43 +0700

    Update hw.cpp

--- hw.cpp
+++ hw.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main() {
 	//		 VV--------- 1 2 3 4 5 6 7 8 9 ..... 40
-	char str[40];	//   H e l l o _ _ _ _       _
+	char str[40];	//           H e l l o _ _ _ _ ..... _     
 	int lenStr;
 
 	cout << "Input String\t: ";
-	cin >> str; // Hello 0 1 2 3 4	<--------
-	// 5								   //
-	lenStr = strlen(str);				  //
-	// out loop because show 1 line		 //
+	cin >> str; // Hello 0 1 2 3 4	<------ //
+	// 5				       //
+	lenStr = strlen(str);		      //
+	// out loop because show 1 line	     //
 	cout << "Reverse String\t: ";       //
-				// 5-1 =  4	- 0 -----> /\	
+		    // 5-1 =  4	- 0 -----> //	
 	for (int i = lenStr-1; i >= 0; i--) {
 		cout << (char)toupper(str[i]);
 	}
